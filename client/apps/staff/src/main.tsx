eb2238d962d38679c082c977e5cd36bae41f93b7
Georgesa98 <georgesalebe0@gmail.com>
2026-02-27 20:36:01 +0300

    feat(sonner): added sonner support.

--- client/apps/staff/src/main.tsx
+++ client/apps/staff/src/main.tsx
@@ -3,11 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@workspace/ui/components/theme-provider";
+import { Toaster } from "@workspace/ui/components/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="theme-ui">
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>,
 );
