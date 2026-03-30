7ecc9edf9a5411eb542e1b09a6b6bbe6895c711d
Georgesa98 <georgesalebe0@gmail.com>
2026-03-30 23:22:53 +0300

    refactor(web): moved styles out of src directory.

--- client/apps/web/app/layout.tsx
+++ client/apps/web/app/layout.tsx
@@ -1,35 +1,35 @@
 import { Plus_Jakarta_Sans, Work_Sans } from "next/font/google";
 
 import "@workspace/ui/globals.css";
-import "../src/styles/theme.css";
+import "@/styles/theme.css";
 import { Providers } from "@/components/providers";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  variable: "--font-plus-jakarta-sans",
-  display: "swap",
+    subsets: ["latin"],
+    variable: "--font-plus-jakarta-sans",
+    display: "swap",
 });
 
 const workSans = Work_Sans({
-  subsets: ["latin"],
-  variable: "--font-work-sans",
-  display: "swap",
+    subsets: ["latin"],
+    variable: "--font-work-sans",
+    display: "swap",
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html
-      className={`${plusJakartaSans.variable} ${workSans.variable} `}
-      lang="en"
-      suppressHydrationWarning
-    >
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html
+            className={`${plusJakartaSans.variable} ${workSans.variable} `}
+            lang="en"
+            suppressHydrationWarning
+        >
+            <body>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
