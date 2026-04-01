64ced775582fdd29f698858e8330dfc3d48a43c0
Georgesa98 <georgesalebe0@gmail.com>
2026-04-01 15:12:00 +0300

    fix(seed): enforced app tp use local image store in development env.

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,3 +4,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 DATABASES = {"default": env.db("TEST_DATABASE_URL")}
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Override S3 storage with local filesystem for development
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
