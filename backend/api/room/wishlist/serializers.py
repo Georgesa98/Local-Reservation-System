758bb9cf881c8351f82138a389e70baff5b69a25
Georgesa98 <georgesalebe0@gmail.com>
2026-04-09 16:56:08 +0300

    fix(wishlist): added the fields to WishlistSerializer.

--- backend/api/room/wishlist/serializers.py
+++ backend/api/room/wishlist/serializers.py
@@ -5,4 +5,5 @@ from .models import Wishlist
 class WishlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wishlist
+        fields = "__all__"
         read_only_fields = ["id", "user", "room" "created_at"]
